9efa547ddd770a7c5c1bd2a342d0f8b1c16e1355
ingalls <ingalls@protonmail.com>
2018-08-03 14:42:46 -0400

    Add cli wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use] extern crate clap;
+extern crate serde_json;
+
+use std::io::{BufRead, BufReader};
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let cli_cnf = load_yaml!("cli.yml");
+    let matched = App::from_yaml(cli_cnf).get_matches();
 }
